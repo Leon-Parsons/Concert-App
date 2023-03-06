df328e8bfedf08bbfe6479713f08c0317c845187
Leon Parsons <MY_NAME@example.com>
2023-03-06 19:39:20 +1300

    Basic Tailwind setup /element layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Musician from './components/Musician';
 import { useState } from 'react';
 
@@ -8,19 +8,26 @@ function App() {
   return (
     <div className="App">
       {showMusicians ? (
-      <div>
-        <input type='text' onChange={(e)=>{
+      <>
+        <input className="rounded-lg" type='text' onChange={(e)=>{
           console.log(e.target.value);
           setTourName(e.target.value);
         }}/>
-        <Musician name={"Metallica"} tourName={tourName}/>
-        <Musician name="Run The Jewels"/>
-        <Musician name="Living Color"/>
-      </div>
+        <div className="flex flex-wrap justify-center">
+          <Musician name="Metallica" tourName={tourName} img="https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg"/>
+          <Musician name="Run The Jewels" tourName="Run Em"/>
+          <Musician name="Living Color"/>
+          <Musician name="Metallica" tourName={tourName}/>
+          <Musician name="Run The Jewels" tourName="Run Em"/>
+          <Musician name="Living Color"/>
+          <Musician name="Metallica" tourName={tourName}/>
+          <Musician name="Run The Jewels" tourName="Run Em"/>
+          <Musician name="Living Color"/>
+        </div>
+      </>
        ) : (
     <p>You cannot see these</p>
-  )
-  }
+  )}
     </div>
   );
 }
