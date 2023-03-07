ea40e2ada79064057b764a19b5bb6a942932bc88
Leon Parsons <MY_NAME@example.com>
2023-03-07 16:08:58 +1300

    Basic edit Musician pop-up functionality

--- src/components/Musician.js
+++ src/components/Musician.js
@@ -1,3 +1,5 @@
+import EditMusician from "./EditMusician";
+
 function Musician(props) {
     return (
         <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
@@ -11,8 +13,10 @@ function Musician(props) {
         {props.tourName}
         </p>
         </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-            Message</button>
+
+        <EditMusician />
+
+    
         </div>
     </div>
     );
