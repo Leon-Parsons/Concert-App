43046fdd78f0e661a543626d8ca6aa82a44f298f
Leon Parsons <MY_NAME@example.com>
2023-03-07 14:53:16 +1300

    Restructures 'musician' data to be array elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import './index.css';
 import Musician from './components/Musician';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [tourName, setTourName] = useState('Tour Name Not Specified')
+  const [musicians, setMusicians] = useState(
+    [
+      {
+        name: "Metallica", 
+        tourName: "", 
+        img: "https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg" 
+      },
+      {
+        name: "Run The Jewels", 
+        tourName: "Run Em", 
+        img: "https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg" 
+      },
+      {
+        name: "Living Color", 
+        tourName: "Greatest Hits", 
+        img: "https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg" 
+      },
+      {
+        name: "Justin Beiber", 
+        tourName: "Believe Tour", 
+        img: "https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg" 
+      },
+      {
+        name: "Gojira", 
+        tourName: "Depths Tour", 
+        img: "https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg" 
+      },
+      {
+        name: "Taylor Swift", 
+        tourName: "Reputation Tour", 
+        img: "https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg" 
+      }
+    ]
+  )
+
   const showMusicians = true;
   return (
     <div className="App">
@@ -14,15 +50,16 @@ function App() {
           setTourName(e.target.value);
         }}/>
         <div className="flex flex-wrap justify-center">
-          <Musician name="Metallica" tourName={tourName} img="https://cdn.pixabay.com/photo/2016/11/23/15/48/guitar-1853661_960_720.jpg"/>
-          <Musician name="Run The Jewels" tourName="Run Em"/>
-          <Musician name="Living Color"/>
-          <Musician name="Metallica" tourName={tourName}/>
-          <Musician name="Run The Jewels" tourName="Run Em"/>
-          <Musician name="Living Color"/>
-          <Musician name="Metallica" tourName={tourName}/>
-          <Musician name="Run The Jewels" tourName="Run Em"/>
-          <Musician name="Living Color"/>
+          {musicians.map((musician) => {
+            return (
+              <Musician 
+                key={uuidv4()}
+                name={musician.name} 
+                tourName={musician.tourName} 
+                img={musician.img}
+            />
+            );
+          })}
         </div>
       </>
        ) : (
